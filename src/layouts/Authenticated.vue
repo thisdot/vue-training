3772f1bd76a0cb7c2ca6a7684b752dac5a732d1f
Bilal Haidar <bhaidar@gmail.com>
2020-06-03 18:55:31 +0300

    Code Refactor

--- src/layouts/Authenticated.vue
+++ src/layouts/Authenticated.vue
@@ -24,16 +24,16 @@ export default {
 </script>
 
 <style lang="scss">
-nav {
-  padding: 30px;
+/*nav {*/
+/*  padding: 30px;*/
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+/*  a {*/
+/*    font-weight: bold;*/
+/*    color: #2c3e50;*/
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+/*    &.router-link-exact-active {*/
+/*      color: #42b983;*/
+/*    }*/
+/*  }*/
+/*}*/
 </style>
