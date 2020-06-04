a63fc7d6ba0d600eacd90e7ddd5ed938595f617a
Bilal Haidar <bhaidar@gmail.com>
2020-06-04 20:07:21 +0300

    Add buttons to Dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,9 +1,10 @@
 <template>
   <Authenticated page-sub-title="Dashboard">
+    <ul>
+      <li><router-link to="#" class="link">Category 1</router-link></li>
+      <li><router-link to="#" class="link">Create New</router-link></li>
+    </ul>
     <router-view />
-    <template #footer>
-      <p>© 2020 This Dot. All Rights Reserved.</p>
-    </template>
   </Authenticated>
 </template>
 
@@ -16,3 +17,19 @@ export default {
   components: { Authenticated },
 };
 </script>
+
+<style lang="scss" scoped>
+ul {
+  display: block;
+  list-style: none;
+}
+
+li {
+  display: inline-block;
+  margin-right: 20px;
+
+  border: 1px solid $primary;
+  border-radius: 5px;
+  padding: 10px;
+}
+</style>
