edd38b133c14991bc9d6ba59020983531d74ec86
Bilal Haidar <bhaidar@gmail.com>
2020-06-07 00:56:13 +0300

    Sign-in user via vuex

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,10 +1,28 @@
 export default {
   state: {
-    name: '',
+    username: localStorage.getItem('username') || '',
     peferredLanguage: 'en',
-    isLoggedIn: false,
   },
-  mutations: {},
-  actions: {},
+  getters: {
+    isLoggedIn: state => !!state.username,
+  },
+  mutations: {
+    SET_NAME(state, username) {
+      state.name = username;
+    },
+  },
+  actions: {
+    login({ commit }, username) {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          localStorage.setItem('username', username);
+
+          commit('SET_NAME', username);
+
+          resolve();
+        }, 500);
+      });
+    },
+  },
   modules: {},
 };
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,25 @@
 <template>
   <Static title="Home">
-    <p>Please login</p>
-    <router-link to="/login">Login</router-link>
+    <div v-if="!isLoggedIn">
+      <p>Please login</p>
+      <router-link to="/login">Login</router-link>
+    </div>
+    <div v-else>
+      <p>Welcome</p>
+    </div>
   </Static>
 </template>
 
 <script>
 // @ is an alias to /src
 import Static from '@/layouts/Static';
+import { mapGetters } from 'vuex';
 
 export default {
   name: 'Home',
   components: { Static },
+  computed: {
+    ...mapGetters(['isLoggedIn']),
+  },
 };
 </script>
