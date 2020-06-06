0c6d4ac35737bee99f9b121d35c07dc510f3efc1
Bilal Haidar <bhaidar@gmail.com>
2020-06-06 17:58:35 +0300

    Add about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,12 +1,53 @@
 <template>
-  <Static title="About">This is an about page</Static>
+  <Static title="About">
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel cursus
+      odio. In vel molestie ipsum. Integer vestibulum nisl vel sagittis
+      placerat. Cras ac condimentum augue. Fusce eget iaculis libero. Curabitur
+      congue ante vitae turpis pretium, a ultricies ligula egestas. Pellentesque
+      suscipit, tortor id aliquam lacinia, nulla massa placerat ex, sit amet
+      rutrum est nibh sed ipsum. Nulla euismod felis nulla, et fermentum nisl
+      euismod eget. Nullam quis ligula nunc. Vivamus semper est posuere nulla
+      efficitur ultricies. Phasellus eget semper mi. Mauris lacinia a urna quis
+      imperdiet. Quisque tincidunt neque lorem, tempor gravida diam tristique
+      ut.
+    </p>
+
+    <p>
+      Duis ac nibh malesuada, aliquet lectus eget, sollicitudin ante. Donec
+      vitae tortor rhoncus, bibendum arcu vel, commodo massa. Pellentesque
+      interdum ante a urna iaculis, lacinia vestibulum turpis varius. Fusce
+      pretium dui ut nisl pulvinar malesuada. Fusce scelerisque elementum
+      tellus, vitae blandit sapien varius in. Vestibulum ullamcorper consequat
+      orci id venenatis. Fusce sagittis libero id felis tincidunt convallis.
+      Donec et mollis nisi. Sed ac lacus erat.
+    </p>
+
+    <p>
+      Praesent et pharetra nibh. Morbi vel hendrerit nisl. Curabitur porttitor
+      cursus mauris sit amet laoreet. Nam feugiat malesuada nisl in suscipit.
+      Sed at leo turpis. Nam lacinia ut lectus vulputate dignissim. Integer
+      volutpat sem non lacus luctus efficitur. Praesent lobortis nisi vehicula
+      placerat sagittis. Sed ac maximus ipsum. Nam volutpat risus eget odio
+      tristique, sit amet molestie urna blandit. Suspendisse ut euismod augue.
+      Suspendisse lectus magna, iaculis vitae sapien et, ultricies dapibus
+      neque. Integer imperdiet sit amet magna nec molestie. Nulla at ornare
+      mauris. Integer eu est porttitor, commodo diam eget, luctus ligula.
+    </p>
+  </Static>
 </template>
 
 <script>
-import Static from "@/layouts/Static";
+import Static from '@/layouts/Static';
 
 export default {
-  name: "About",
-  components: { Static }
+  name: 'About',
+  components: { Static },
 };
 </script>
+
+<style lang="scss" scoped>
+p {
+  padding-top: 25px;
+}
+</style>
