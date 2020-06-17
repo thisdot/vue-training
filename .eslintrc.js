6b9688e3ccdfef99e7cd632e6d0481d7bc706d99
Simone Cuomo <scuomo1988@gmail.com>
2020-06-17 08:41:23 +0100

    Fix lintin config

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
         indent: ['error', 'tab'],
       },
     ],
+    quotes: [1, 'single'],
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
